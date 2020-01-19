8ff40c4b0f976a66c13ac3780037f43db29c5032
BuildTools <luminai@gmail.com>
2020-01-19 15:37:08 -0500

    second commit

---
+++ src/main/java/com/mycompany/pages/ReadySetExcelPage.java
@@ -0,0 +1,39 @@
+package com.mycompany.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class ReadySetExcelPage {
+    private WebDriver pagedriver;
+    private String url = "https://www.readysetexcel.net";
+    private WebElement get_in_touch;
+
+    public ReadySetExcelPage(WebDriver driver) {
+        pagedriver = driver;
+    };
+
+    public void go() {
+        pagedriver.get(url);
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    };
+
+    public String getTitle(){
+        return pagedriver.getTitle();
+    }
+
+    public void clickGetInTouchMain(){
+        pagedriver.switchTo().frame(0);
+        get_in_touch = pagedriver.findElement( By.cssSelector(".button a.btn.btn-primary"));
+        get_in_touch.click();
+    }
+
+
+
+
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,52 @@
+package com.mycompany.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.JavascriptExecutor;
+import com.mycompany.pages.ReadySetExcelPage;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    public String url = "https://www.readysetexcel.net/";
+    public WebDriver driver;
+    public ReadySetExcelPage page;
+
+    @BeforeTest
+    public void setBaseUrl() {
+        System.setProperty("webdriver.chrome.driver", "/Users/luisrueda/Dropbox/Ready_Set_Excel/ready-set-excel-test/target/chromedriver");
+        driver = new ChromeDriver();
+        page = new ReadySetExcelPage(driver);
+        page.go();
+    }
+
+
+    @AfterTest
+    public void endSession(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void verifyHomePageTitle() {
+        String title = page.getTitle();
+        Assert.assertEquals("Ready.Set.Excel.", title);
+    }
+
+    @Test
+    public void verifyGetInTouchMain(){
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        Long value1 = (Long) executor.executeScript("return window.pageYOffset;");
+        page.clickGetInTouchMain();
+        Long value2 = (Long) executor.executeScript("return window.pageYOffset;");
+        System.out.println("value1=" + value1.toString());
+        System.out.println("value2=" + value2.toString());
+        Assert.assertNotEquals(value1, value2);
+    }
+}
